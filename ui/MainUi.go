f6585b4caed71968ae30b800837fa5724b0a30e9
hfdxiaoyu <1306933520@qq.com>
2022-10-06 16:20:49 +0800

    实现定时弹窗提醒功能

--- ui/MainUi.go
+++ ui/MainUi.go
@@ -2,69 +2,83 @@ package ui
 
 import (
 	"EyeshieldTime/util"
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"strconv"
+	"time"
 )
 
-func MainWindow(){
+func MainWindow() {
 	a := app.New()
 	w := a.NewWindow("护眼助手")
+	w1 := a.NewWindow("护眼提示")
 	//设置窗口大小
-	w.Resize(fyne.NewSize(100,100))
+	w.Resize(fyne.NewSize(100, 100))
 
 	//时间标签
 	bHour := widget.NewLabel("时：")
 	bMin := widget.NewLabel("分：")
 	bSecods := widget.NewLabel("秒：")
 
-
-	//数据绑定
-	//bdHour := binding.NewString()
-	//bdMin := binding.NewString()
-	//bdSecods := binding.NewString()
-
-	//输入框 输入时间
-	//hour := widget.NewEntryWithData(bdHour) //时
-	//min := widget.NewEntryWithData(bdMin) //分
-	//secods := widget.NewEntryWithData(bdSecods) //秒
-
 	//下拉选择框 输入时间
-	xHour := widget.NewSelectEntry(util.NumStringBuild(24)) //时
-	xMin := widget.NewSelectEntry(util.NumStringBuild(60)) //时
-	xSecods := widget.NewSelectEntry(util.NumStringBuild(60)) //时
+	xHour := widget.NewSelectEntry(util.NumStringBuild(24))   //时
+	xMin := widget.NewSelectEntry(util.NumStringBuild(60))    //分
+	xSecods := widget.NewSelectEntry(util.NumStringBuild(60)) //秒
 
-	//xmin := widget.NewEntryWithData(bdMin) //分
-	//xsecods := widget.NewEntryWithData(bdSecods) //秒
-
+	//用来控制定时器
+	var da = make(chan int)
 
 	//开始按钮
 	start := widget.NewButton("开始", func() {
-		dialog.NewConfirm("提示","护眼提示", func(b bool) {
+		//把时间转为int类型
+		inxHour, _ := strconv.Atoi(xHour.Text)
+		inxMin, _ := strconv.Atoi(xMin.Text)
+		inxSecods, _ := strconv.Atoi(xSecods.Text)
 
-		},w)
+		go DateDecres(inxHour, inxMin, inxSecods, da, w1)
 	})
 
 	//停止按钮
 	stop := widget.NewButton("停止", func() {
-
+		fmt.Println("停止")
+		da <- 1
 	})
 
 	//布局
 	//先把元素存入盒子
 	//网格布局
-	box := container.NewGridWithColumns(6,bHour,xHour,bMin,xMin,bSecods,xSecods)
-	bubox := container.NewHBox(start,stop)
+	box := container.NewGridWithColumns(6, bHour, xHour, bMin, xMin, bSecods, xSecods)
+	bubox := container.NewHBox(start, stop)
 
 	//边布局
 	content := container.New(
-		layout.NewBorderLayout(box,nil,nil,bubox),
-		box,bubox,
-		)
+		layout.NewBorderLayout(box, nil, nil, bubox),
+		box, bubox,
+	)
 	w.SetContent(content)
 	w.ShowAndRun()
 }
 
+//计时器
+func DateDecres(hours int, min int, secods int, da chan int, wi fyne.Window) {
+	//time.Duration将int类型转为时间类型
+	fhour := time.Duration(hours) * time.Second * 60 * 60
+	fmin := time.Duration(min) * time.Second * 60
+	fsecods := time.Duration(secods) * time.Second
+
+	timer := time.NewTimer(fhour + fmin + fsecods)
+	select {
+	case <-timer.C:
+		fmt.Printf("运行时间：%d 小时 %d 分钟 %d 秒\n", hours, min, secods)
+		PopUpWindow(wi)
+		break
+	case <-da:
+		timer.Stop()
+		break
+	}
+
+}
--- ui/WidgetUi.go
+++ ui/WidgetUi.go
@@ -1,5 +1,25 @@
 package ui
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/dialog"
+)
+
 //一些其它的窗体
 
+//弹窗
+func PopUpWindow(window fyne.Window) {
 
+	//设置弹窗大小
+	window.Resize(fyne.NewSize(230, 150))
+	confirm := dialog.NewConfirm("护眼提示", "休息一下眼睛吧", func(b bool) {
+		if b {
+			window.Hide()
+		} else {
+			window.Hide()
+		}
+	}, window)
+
+	confirm.Show()
+	window.Show()
+}
